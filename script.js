9242b553a3b5218538fb97cc0b230ae2c37cfa08
ivanaca777 <100292432+ivanaca777@users.noreply.github.com>
2023-08-30 01:19:58 -0300

    se configura regex

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function openMenu() {
-    let menu = document.querySelector('.menu')
-    if (menu.style.display == 'flex') {
-        menu.style.display = 'none';
-    }
-    else {
-        menu.style.display = 'flex';
-    }
+  let menu = document.querySelector('.menu')
+  if (menu.style.display == 'flex') {
+      menu.style.display = 'none';
+  }
+  else {
+      menu.style.display = 'flex';
+  }
 }
 
 //let formulario = document.querySelector("form");
@@ -13,31 +13,43 @@ function openMenu() {
 //regex:
 let regex = /^[A-Za-z]\D*$/gm;
 
-//div para el error de nombre
+//div para el error de nombre y apellido
 let errorNombre = document.querySelector('.errorNombre');
+let errorApellido = document.querySelector('.errorApellido');
 
+//evento submit
 formulario.addEventListener("submit", (event)=>{
   event.preventDefault();
 
   let nombre = event.target[0].value;
   let apellido = event.target[1].value;
+  //pide validar nombre con regex, limpiar si lo hace o dar error si no valida 
+  if (validar(nombre)){   
+   nombre = limpiar(nombre);
+  }
+  else {
+    errorNombre.textContent = "Hay caracteres incorrectos o falta completar el campo";
+  }
 
-  nombre = validar(nombre);
-  nombre = limpiar(nombre);
-  apellido = validar(apellido)
-  apellido = limpiar(apellido);
-  console.log(nombre,apellido);
+  //pide validar apellido con regex, limpiar si lo hace o dar error si no valida
+  if (validar(apellido)){   
+    apellido = limpiar(apellido);
+  }
+  else {
+    errorApellido.textContent = "Hay caracteres incorrectos o falta completar el campo";
+  }
+
+  //console.log(nombre,apellido);
 });
 
 function validar(inputValue) {
   let valorLimpio = inputValue.trim();
-
   if(valorLimpio.match(regex)) {
-    return valorLimpio
+    return true;
   }
   else {
-    errorNombre.textContent = "Hay caracteres incorrectos";
-  }
+    return false;
+  }  
 }
 
 function limpiar(inputValue) {
@@ -46,7 +58,7 @@ function limpiar(inputValue) {
   if(valorLimpio[0] === valorLimpio[0].toUpperCase()) {
     return valorLimpio
   }
-  
+
   else {
     let valorReconstruido = "";
     for(let i = 0; i < valorLimpio.length; i++){
@@ -60,5 +72,3 @@ function limpiar(inputValue) {
     return valorReconstruido;
   }
 }
-
-
